785dc9956d4dbdf93751772919965736240d740e
최선혁 <wafy75@gmail.com>
2022-10-11 13:10:51 +0900

    테스트 임베디드 모드 dB 설정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,13 +2,17 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -24,6 +28,18 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 테이터베이스 초기화");
+		DriverManagerDataSource datSource = new DriverManagerDataSource();
+		datSource.setDriverClassName("org.h2.Driver");
+		datSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		datSource.setUsername("sa");
+		datSource.setPassword("");
+		return datSource;
 	}
 
 }
